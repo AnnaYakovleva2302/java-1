c59076db3bd3156dad01c9bf033193b7cc4419b2
Anna Yakovleva <_bel4onok_@mail.ru>
2021-08-08 17:20:51 +0300

    Add report.md

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4539476889010140";
+        String number = "6304414057957501";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
